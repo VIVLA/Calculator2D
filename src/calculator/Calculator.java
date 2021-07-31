e78eb0cac8d7a6904f5394646a675affe0cf4f5a
Sandhopper <Sanya__1981@mail.ru>
2021-07-31 14:21:05 +0300

    Changed discription in java files

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,23 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculator;
 
-/**
- *
- * @author SK-2018
- */
 public class Calculator {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
         new Window();
-         
     }
-    
 }
--- src/calculator/Equations.java
+++ src/calculator/Equations.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculator;
 
 import java.awt.BorderLayout;
@@ -13,10 +8,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-/**
- *
- * @author SK-2018
- */
 public class Equations extends JPanel {
 
     public JButton display;
@@ -113,7 +104,5 @@ public class Equations extends JPanel {
             else if (lastCommand.equals("/")) result /= x;
             display.setText(" " + result);
         }
-        
     }
-    
 }
